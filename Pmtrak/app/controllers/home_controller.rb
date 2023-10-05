6ee49ac0d895c794ae633d5ed7c1818d494cba3d
rdhamal <rdhamal@ncsu.edu>
2023-10-05 15:20:32 -0400

    Logout for root fixed

--- Pmtrak/app/controllers/home_controller.rb
+++ Pmtrak/app/controllers/home_controller.rb
@@ -1,14 +1,14 @@
 class HomeController < ApplicationController
   skip_before_action :authorized, only: [:index]
   def index
-    if (@current_user.class == Admin)
+    if (current_user.class == Admin)
       respond_to do |format|
-        format.html { redirect_to admin_path(@current_user), notice: "Admin page" }
+        format.html { redirect_to admin_path(current_user), notice: "Admin page" }
       end
       
-    elsif (@current_user.class == Passenger)
+    elsif (current_user.class == Passenger)
       respond_to do |format|
-        format.html { redirect_to passenger_path(@current_user), notice: "Admin page" }
+        format.html { redirect_to passenger_path(current_user), notice: "Admin page" }
       end
     else
       session[:admin_id] = nil
